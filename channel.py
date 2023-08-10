5c9e0924da73e6ca04c1192213de9fb06ebfcd38
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-08-10 21:27:53 +0500

    Create channel.py

---
+++ channel.py
@@ -0,0 +1,6 @@
+import telebot
+bot = telebot.TeleBot("6344256672:AAGy5JpT0dTlom0562o3MgO1G-cm54MtAtQ")
+@bot.message_handler(commands=['channel'])
+def chennel(message):
+	bot.send_message(message.chat.id, "Наш канал: @NodeStresserChannel")
+bot.polling(none_stop=True)
