b9b9256e98c14fd5dafa4ff0f00f356148e8a57f
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2026-03-30 21:52:07 +0500

    Update channel.py

--- channel.py
+++ channel.py
@@ -1,6 +1,1 @@
-import telebot
-bot = telebot.TeleBot("6344256672:AAGy5JpT0dTlom0562o3MgO1G-cm54MtAtQ")
-@bot.message_handler(commands=['channel'])
-def channel(message):
-	bot.send_message(message.chat.id, "Наш канал: @NodeStresserChannel")
-bot.polling(none_stop=True)
+# not found
